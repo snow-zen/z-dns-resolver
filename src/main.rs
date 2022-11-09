382f081da10a8715273159a3852a349030aacf9b
snow-zen <2372912451zxj@gmail.com>
2022-11-09 18:04:25 +0800

    feat: Use udp socket connect to dns server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use std::net::{Ipv4Addr, UdpSocket};
+use std::str::FromStr;
 
 #[derive(Parser)]
 struct Cli {
@@ -7,7 +9,18 @@ struct Cli {
     domain: String,
 }
 
-fn main() {
+const DNS_PROTOCOL_DEFAULT_PORT: u16 = 53;
+
+fn main() -> std::io::Result<()> {
     let args: Cli = Cli::parse();
-    println!("{}", args.domain)
+    println!("parsing {}...", args.domain);
+    let socket = UdpSocket::bind((Ipv4Addr::UNSPECIFIED, 0))?;
+    println!("local addr: {}", socket.local_addr().unwrap());
+
+    let mut buf = [0; 32];
+    let result = socket.connect((
+        Ipv4Addr::from_str("114.114.114.114").unwrap(),
+        DNS_PROTOCOL_DEFAULT_PORT,
+    ));
+    Ok(())
 }
