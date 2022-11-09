c939fdb7b6a192e9b3e01bedf1a660886c4bc4f1
snow-zen <2372912451zxj@gmail.com>
2022-11-09 18:05:23 +0800

    feat: Support make question header

--- src/main.rs
+++ src/main.rs
@@ -24,3 +24,43 @@ fn main() -> std::io::Result<()> {
     ));
     Ok(())
 }
+
+/// 制作 question 头
+fn make_question_header(
+    query_id: u16,
+    flag: u16,
+    num_questions: u16,
+    num_answers: u16,
+    num_auth: u16,
+    num_additional: u16,
+) -> Vec<u8> {
+    [
+        query_id.to_be_bytes(),
+        flag.to_be_bytes(),
+        num_questions.to_be_bytes(),
+        num_answers.to_be_bytes(),
+        num_auth.to_be_bytes(),
+        num_additional.to_be_bytes(),
+    ]
+    .concat()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::make_question_header;
+
+    fn to_hex_str(bytes: Vec<u8>) -> String {
+        let mut result = String::new();
+        for byte in bytes {
+            result.push_str(format!("{:02x}", byte).as_str())
+        }
+        result
+    }
+
+    #[test]
+    fn test_make_question_header() {
+        let question_header = make_question_header(0xb962, 0x0100, 0x0001, 0x0000, 0x0000, 0x0000);
+        assert_eq!(question_header.len(), 12);
+        assert_eq!(to_hex_str(question_header), "b96201000001000000000000");
+    }
+}
