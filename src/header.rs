7499b2d65d7a266fe248ec8d1314fe272ffdefc5
snow-zen <2372912451zxj@gmail.com>
2022-11-17 18:17:47 +0800

    feat: adjust header

--- src/header.rs
+++ src/header.rs
@@ -3,12 +3,27 @@ use bincode::{Decode, Encode};
 /// DNS 查询结构 Header 部分
 #[derive(Encode, Decode)]
 pub struct Header {
-    query_id: u16,
-    flag: u16,
-    num_questions: u16,
-    num_answers: u16,
-    num_auth: u16,
-    num_additional: u16,
+    /// 标识符
+    id: u16,
+    /// 用于指定当前消息是 query(0) 还是 response(1)
+    qr: bool,
+    /// 指定消息中的查询类型，其值有以下定义
+    /// 0       = 标准查询（QUERY）
+    /// 1       = 反向查询（IQUERY）
+    /// 2       = 服务器状态请求（STATUS）
+    /// 3-15    = 保留值
+    opcode: u8,
+    /// 权威答案，该位在响应中有效，表示响应名称服务器
+    ///
+    /// 注意：[Answer] 部分的内容由于别名可能有多个所有者名称。
+    /// aa 对应与查询名称匹配的名称，或者 [Answer] 部分第一个所有者名称。
+    aa: u8,
+
+    // flag: u16,
+    // num_questions: u16,
+    // num_answers: u16,
+    // num_auth: u16,
+    // num_additional: u16,
 }
 
 impl Header {
@@ -21,7 +36,7 @@ impl Header {
         num_additional: u16,
     ) -> Self {
         Self {
-            query_id,
+            id: query_id,
             flag,
             num_questions,
             num_answers,
@@ -60,7 +75,7 @@ mod tests {
         let (q_header, number_size) = deserialize_to_struct::<Header>(&bytes);
 
         assert_eq!(number_size, 12);
-        assert_eq!(q_header.query_id, 0xb962);
+        assert_eq!(q_header.id, 0xb962);
         assert_eq!(q_header.flag, 0x0100);
         assert_eq!(q_header.num_questions, 0x0001);
         assert_eq!(q_header.num_answers, 0x0000);
